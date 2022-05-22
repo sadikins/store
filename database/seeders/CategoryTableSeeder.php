03d55f3e61a430e01b488da16110c5bacb5baf3a
Sadikins <msadikins1989@gmail.com>
2022-05-22 09:46:23 +0700

    add default image to category seeder

--- database/seeders/CategoryTableSeeder.php
+++ database/seeders/CategoryTableSeeder.php
@@ -19,70 +19,70 @@ class CategoryTableSeeder extends Seeder
         $category = [
             
             [
-                'image' => '',
+                'image' => 'default.png',
                 'name' => 'Drinks',
                 'description' => 'Jenis minuman berenergi, obat, soda, jus',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'image' => '',
+                'image' => 'default.png',
                 'name' => 'Canned food',
                 'description' => 'Makanan yang diawetkan dalam kaleng',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'image' => '',
+                'image' => 'default.png',
                 'name' => 'Bakery',
                 'description' => 'Makanan yang terbuat dari roti',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'image' => '',
+                'image' => 'default.png',
                 'name' => 'Noodles',
                 'description' => 'Mie dan jenisnya',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'image' => '',
+                'image' => 'default.png',
                 'name' => 'Dry good',
                 'description' => 'Makanan yang diawetkan dengan cara dikeringkan',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'image' => '',
+                'image' => 'default.png',
                 'name' => 'Syrup',
                 'description' => 'Minuman sari buah',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'image' => '',
+                'image' => 'default.png',
                 'name' => 'Candies dan chocolate',
                 'description' => 'Permen dan coklat',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'image' => '',
+                'image' => 'default.png',
                 'name' => 'Seasoning',
                 'description' => 'Bumbu Dapur',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'image' => '',
+                'image' => 'default.png',
                 'name' => 'Local basic',
                 'description' => '9 Bahan Pokok',
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                'image' => '',
+                'image' => 'default.png',
                 'name' => 'Milk and milk product',
                 'description' => 'Susu untuk bayi hingga dewasa',
                 'created_at' => now(),
